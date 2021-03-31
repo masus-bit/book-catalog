b173d05d3a4c728d6c87be75c780a1547e3304a1
masus-bit <maxumxumxum@gmail.com>
2021-03-31 23:36:15 +0300

    adeed types.ts

---
+++ front/src/store/app/types.ts
@@ -0,0 +1,23 @@
+import {Action as ActionRedux} from 'redux'
+import { App } from '../../types/app'
+import { Auth } from '../../types/auth'
+import { Thunk } from '../../types/base'
+import { AppAction } from './appAction'
+export declare namespace AppState{
+    interface State{
+        readonly loading:boolean;
+        readonly accessToken: string;
+        readonly refreshToken:string;
+        readonly errorText: string;
+    }
+    namespace Action{
+        type Fetch = ActionRedux<AppAction.Fetch> & {payload?:undefined}
+        type FetchSuccess = ActionRedux<AppAction.FetchSuccess> & {payload: App.Token}
+        type FetchError = ActionRedux<AppAction.FetchError> & {payload:string}
+
+        type All=Fetch | FetchSuccess | FetchError
+    }
+    interface ActionThunk{
+        appLogin: Thunk<Auth.Login.Params>
+    }
+}
